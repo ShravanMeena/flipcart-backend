e2f683f5fda6a28cba4f56e701be12b1eb3bf4c5
Shravan <41790742+ShravanMeena@users.noreply.github.com>
2020-12-30 15:23:57 +0530

    asd

---
+++ src/server.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+
+const env = require("dotenv");
+const app = express();
+
+// env variable
+env.config();
+
+mongoose
+  .connect(
+    `mongodb+srv://${process.env.MONGO_DB_USER}:${process.env.PASSWORD}@cluster0.ugfuj.mongodb.net/${process.env.DATABASE_NAME}?retryWrites=true&w=majority`,
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }
+  )
+  .then(() => {
+    console.log("Database connected");
+  });
+
+// app.use(bodyParser());
+
+app.listen(process.env.PORT, () => {
+  console.log(`Server is running on port ${process.env.PORT}`);
+});
